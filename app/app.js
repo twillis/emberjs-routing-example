0159850a21c8659a8adfc55decd9c75b26b597d0
Tom Willis <tom.willis@gmail.com>
2013-01-06 07:29:28 -0500

    initial app.js and application template proving the build chain is working

--- app/app.js
+++ app/app.js
@@ -1,5 +1,16 @@
+/*global Ember, require*/
 require('dependencies/jquery-1.8.3');
 require('dependencies/handlebars-runtime');
 require('dependencies/ember');
 require('dependencies/compiled/templates');
 
+
+//create app and app namespace used by all other modules
+
+window.App = Ember.Application.create({});
+
+//require other modules to compose the application
+require("app/models");
+require("app/views");
+require("app/controllers");
+require("app/router");
